35f6d13f040a1889b9036080c3ff082570fda981
cynthiaveruca <68599460+cynthiaveruca@users.noreply.github.com>
2021-08-12 19:24:20 +0530

    Start of the Repository
    some basic elements like, textboxes, buttons, windows, focus interactions etc.

---
+++ tOS/input.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ananda on 7/19/2021.
+ */
+let inputField = document.getElementById("entryField");
+let suggestDialog = document.getElementById("entryField");
+
---
+++ tOS/native-textbox.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ananda on 7/19/2021.
+ */
+let inputRoot = document.querySelectorAll(".text-root");
+inputRoot.forEach(function (el) {
+    el.attachShadow(true);
+});
